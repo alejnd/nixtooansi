449d209f184b9c00e00a0ee1aaa5aff6b0cae983
Alejandro Murciano <alemail@gmail.com>
2017-02-15 15:08:31 +0000

    network config changes

--- configuration.nix
+++ configuration.nix
@@ -10,12 +10,8 @@
     { 
       networking.networkmanager.enable = true;
       services.sshd.enable = true;
-      networking.firewall.enable = false;
       networking.firewall.allowedTCPPorts = [ 5000 22 ];
       networking.firewall.allowPing = true;
-      networking.interfaces.eth0.ip4 = [ { address = "192.168.1.200"; prefixLength = 24; } ];
-      networking.defaultGateway = "192.168.1.1";
-      networking.nameservers = [ "8.8.8.8" ];
       
  #     options.services.tooansi = {
  #       enable = mkOption {
